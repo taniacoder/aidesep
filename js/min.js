ffde4928309549ee33e1955499dbb2422541bdc6
taniacoder <tania.cuyamedina@gmail.com>
2017-09-24 20:44:14 -0500

    first commit

---
+++ js/min.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+  //Set the carousel options
+  $('#fade-quote-carousel.carousel').carousel({
+    pause: true,
+    interval: 900000,
+    controls: true,
+  });
+});
