6f476d9aa063a23a57025925e00b7bf32a738e08
Tania Cuya Medina <tania.cuyamedina@gmail.com>
2017-10-17 20:50:18 -0500

    slick

--- js/min.js
+++ js/min.js
@@ -1,10 +1,36 @@
-
-
-$(document).ready(function() {
-  //Set the carousel options
-  $('#fade-quote-carousel.carousel').carousel({
-    pause: true,
-    interval: 900000,
-    controls: true,
-  });
+$('.responsive').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 3000,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
