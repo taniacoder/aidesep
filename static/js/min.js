58a3e7f09b7ebcd00f5e0fe01f0a5a11c6ebb29a
Tania Cuya Medina <tania.cuyamedina@gmail.com>
2017-10-19 21:42:30 -0500

    dd

--- static/js/min.js
+++ static/js/min.js
@@ -2,9 +2,46 @@ $('.responsive').slick({
   dots: false,
   infinite: true,
   autoplay: true,
-  speed: 3000,
+  speed: 5000,
   slidesToShow: 5,
   slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.responsive2').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
