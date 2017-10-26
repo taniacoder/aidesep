d87313cddf7c75a91f97776dd99d329dcd8b85b0
Tania Cuya Medina <tania.cuyamedina@gmail.com>
2017-10-25 20:02:42 -0500

    orden

--- static/modules/header.php
+++ static/modules/header.php
@@ -1,79 +1,69 @@
-		section.header
-			.container
-				.row
-					.col-md-4.col-xs-12.col-sm-12
-						img(width='70%' src='img/home/logo.png')
-					.col-md-8.col-xs-12.col-sm-12
-						.head-right
-							.logo-m-seeker
-								.redes
-									.item
-										.icon-circle
-											i.fa.fa-facebook(aria-hidden="true")
-										.icon-circle
-											i.fa.fa-twitter(aria-hidden="true")
-										.icon-circle
-											i.fa.fa-youtube(aria-hidden="true")
-										.icon-circle
-											i.fa.fa-flickr(aria-hidden="true")
-								.search
-									form
-										input(type='text', value='Buscador', onfocus="this.value = '';", onblur="if (this.value == '') {this.value = '';}")
-										input(type='submit', value='BUSCADOR')
-		section.menu
-			nav.navbar.navbar-default
-				.container
-					// Brand and toggle get grouped for better mobile display
-					.navbar-header
-						button.navbar-toggle.collapsed(type='button', data-toggle='collapse', data-target='#bs-example-navbar-collapse-1', aria-expanded='false')
-							span.sr-only Toggle navigation
-							span.icon-bar
-							span.icon-bar
-							span.icon-bar
-						.redes-seeker.collapsed
-							ul.social-in
-								li
-									a(href='#')
-										i.facebook  
-								li
-									a(href='#')
-										i.twitter  
-								li
-									a(href='#')
-										i.youtube  
-								li
-									a(href='#')
-										i.rs  
-					// Collect the nav links, forms, and other content for toggling
-					#bs-example-navbar-collapse-1.collapse.navbar-collapse
-						ul.nav-flex
-							li
-								a(href='#') INICIO
-							li.dropdown
-								a.dropdown-toggle(href='#', data-toggle='dropdown', role='button', aria-haspopup='true', aria-expanded='false')
-									| NOSOTROS
-									span.caret
-								ul.dropdown-menu
-									li
-										a(href='#') QUIENENS SOMOS
-									li
-										a(href='#') NUESTRA HISTORIA
-									li
-										a(href='#') MÍSION Y VÍSION
-									li
-										a(href='#') CONSEJO DIRECTIVO
-									li
-										a(href='#') 
-											|ORGANIZACIONES 
-											br
-											| REGIONALES
-									li
-										a(href='#') QUÉ QUEREMOS
-							li
-								a(href='#') NUESTRO TRABAJO
-							li
-								a(href='#') NOTICIAS
-							li
-								a(href='#') MULTIMEDIA
-							li
-								a(href='#') CONTACTO
+    <!--header-->
+    <section class="header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-xs-12 col-sm-12"><img width="70%" src="img/home/logo.png"></div>
+          <div class="col-md-8 col-xs-12 col-sm-12">
+            <div class="head-right">
+              <div class="logo-m-seeker">
+                <div class="redes">
+                  <div class="item">
+                    <div class="icon-circle"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+                    <div class="icon-circle"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+                    <div class="icon-circle"><i class="fa fa-youtube" aria-hidden="true"></i></div>
+                    <div class="icon-circle"><i class="fa fa-flickr" aria-hidden="true"></i></div>
+                  </div>
+                </div>
+                <div class="search">
+                  <form>
+                    <input type="text" value="Buscador" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}">
+                    <input type="submit" value="BUSCADOR">
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="menu">
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display-->
+          <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            <div class="redes-seeker collapsed">
+              <ul class="social-in">
+                <li><a href="#"><i class="facebook"> </i></a></li>
+                <li><a href="#"><i class="twitter"> </i></a></li>
+                <li><a href="#"><i class="youtube"> </i></a></li>
+                <li><a href="#"><i class="rs"> </i></a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling-->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav-flex">
+              <li><a href="#">INICIO</a></li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">NOSOTROS<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">> QUIENENS SOMOS</a></li>
+                  <li><a href="#">> NUESTRA HISTORIA</a></li>
+                  <li><a href="#">> MÍSION Y VÍSION</a></li>
+                  <li><a href="#">> CONSEJO DIRECTIVO</a></li>
+                  <li><a href="#">
+                       > ORGANIZACIONES <br>REGIONALES</a></li>
+                  <li><a href="#">> QUÉ QUEREMOS</a></li>
+                </ul>
+              </li>
+              <li><a href="#">NUESTRO TRABAJO</a></li>
+              <li><a href="#">NOTICIAS</a></li>
+              <li><a href="#">MULTIMEDIA</a></li>
+              <li><a href="#">CONTACTO</a></li>
+            </ul>
+          </div>
+          <!-- /.navbar-collapse-->
+        </div>
+        <!-- /.container-fluid-->
+      </nav>
+    </section>
